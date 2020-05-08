5addee34563f742e293299e404bbadda983b9b4d
Sohaib Ilyas <contact@sohaibilyas.com>
2020-05-08 23:34:04 +0500

    set response type

--- src/Facebook.php
+++ src/Facebook.php
@@ -130,12 +130,12 @@ class Facebook
         return json_decode($this->response->getBody()->getContents());
     }
 
-    public function toArray()
+    public function toArray(): array
     {
         return json_decode($this->response->getBody()->getContents(), true);
     }
 
-    public function toJson()
+    public function toJson(): string
     {
         return $this->response->getBody()->getContents();
     }
