35dcc4110d59341a4b600dc7b6620dbd5b54594f
Sohaib Ilyas <contact@sohaibilyas.com>
2020-05-17 02:00:53 +0500

    get user posts example added

---
+++ examples/get-user-posts/index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+require './vendor/autoload.php';
+
+use SohaibIlyas\FacebookPhpSdk\Facebook;
+
+$facebook = new Facebook([
+    'app_id' => 'your_app_id_here',
+    'app_secret' => 'your_app_secret_here',
+    'redirect_uri' => 'your_redirect_uri_here'
+]);
+
+if ($facebook->loggedIn() || isset($_SESSION['access_token'])) {
+    if (isset($_SESSION['access_token'])) {
+        $facebook->setAccessToken($_SESSION['access_token']);
+    }
+
+    $_SESSION['access_token'] = $facebook->getAccessToken();
+
+    $response = $facebook->get('/me/feed')->toArray();
+
+    print_r($response);
+} else {
+    echo "<a href='" . $facebook->getLoginUrl(['email', 'user_posts']) . "'>Login with Facebook</a>";
+}
