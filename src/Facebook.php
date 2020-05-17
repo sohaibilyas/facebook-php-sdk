8498b911b7ac5b3e84d073f4e72a84fc49ab9504
Sohaib Ilyas <isohaibilyas@gmail.com>
2020-05-17 13:19:03 +0000

    Apply fixes from StyleCI

--- src/Facebook.php
+++ src/Facebook.php
@@ -19,19 +19,19 @@ class Facebook
 
     public function __construct(array $config = null)
     {
-        if (!$config) {
+        if (! $config) {
             throw new Exception('config array not provided to contructer');
         }
 
-        if (!isset($config['app_id']) || $config['app_id'] == '') {
+        if (! isset($config['app_id']) || $config['app_id'] == '') {
             throw new Exception('app_id not set in config array');
         }
 
-        if (!isset($config['app_secret']) || $config['app_secret'] == '') {
+        if (! isset($config['app_secret']) || $config['app_secret'] == '') {
             throw new Exception('app_secret not set in config array');
         }
 
-        if (!isset($config['redirect_uri']) || $config['redirect_uri'] == '') {
+        if (! isset($config['redirect_uri']) || $config['redirect_uri'] == '') {
             throw new Exception('redirect_uri not set in config array');
         }
 
@@ -60,7 +60,7 @@ class Facebook
 
     public function loggedIn()
     {
-        if (!empty($this->accessToken)) {
+        if (! empty($this->accessToken)) {
             return true;
         }
 
@@ -69,7 +69,7 @@ class Facebook
                 throw new Exception('state token did not match');
             }
 
-            $this->accessToken = json_decode($this->client->get($this->graphVersion . '/oauth/access_token?client_id=' . $this->config['app_id'] . '&client_secret=' . $this->config['app_secret'] . '&redirect_uri=' . $this->config['redirect_uri'] . '&code=' . $_GET['code'])->getBody())->access_token;
+            $this->accessToken = json_decode($this->client->get($this->graphVersion.'/oauth/access_token?client_id='.$this->config['app_id'].'&client_secret='.$this->config['app_secret'].'&redirect_uri='.$this->config['redirect_uri'].'&code='.$_GET['code'])->getBody())->access_token;
 
             return true;
         }
@@ -83,12 +83,12 @@ class Facebook
         $this->state = bin2hex(random_bytes(20));
         $_SESSION['state'] = $this->state;
 
-        return 'https://www.facebook.com/' . $this->graphVersion . '/dialog/oauth?client_id=' . $this->config['app_id'] . '&redirect_uri=' . $this->config['redirect_uri'] . '&scope=' . $permissions . '&state=' . $this->state;
+        return 'https://www.facebook.com/'.$this->graphVersion.'/dialog/oauth?client_id='.$this->config['app_id'].'&redirect_uri='.$this->config['redirect_uri'].'&scope='.$permissions.'&state='.$this->state;
     }
 
     public function post(string $path, array $params, string $accessToken = null)
     {
-        $path = $path[0] == '/' ? $path : '/' . $path;
+        $path = $path[0] == '/' ? $path : '/'.$path;
 
         $this->accessToken = $accessToken ? $accessToken : $this->accessToken;
 
@@ -97,7 +97,7 @@ class Facebook
             $separator = '&';
         }
 
-        $this->response = $this->client->post($this->graphVersion . $path . $separator . 'access_token=' . $this->accessToken, [
+        $this->response = $this->client->post($this->graphVersion.$path.$separator.'access_token='.$this->accessToken, [
             'json' => $params,
         ]);
 
@@ -106,7 +106,7 @@ class Facebook
 
     public function get(string $path, string $accessToken = null)
     {
-        $path = $path[0] == '/' ? $path : '/' . $path;
+        $path = $path[0] == '/' ? $path : '/'.$path;
 
         $this->accessToken = $accessToken ? $accessToken : $this->accessToken;
 
@@ -115,14 +115,14 @@ class Facebook
             $separator = '&';
         }
 
-        $this->response = $this->client->get($this->graphVersion . $path . $separator . 'access_token=' . $this->accessToken);
+        $this->response = $this->client->get($this->graphVersion.$path.$separator.'access_token='.$this->accessToken);
 
         return $this;
     }
 
     public function delete(string $path, string $accessToken = null)
     {
-        $path = $path[0] == '/' ? $path : '/' . $path;
+        $path = $path[0] == '/' ? $path : '/'.$path;
 
         $this->accessToken = $accessToken ? $accessToken : $this->accessToken;
 
@@ -131,7 +131,7 @@ class Facebook
             $separator = '&';
         }
 
-        $this->response = $this->client->delete($this->graphVersion . $path . $separator . 'access_token=' . $this->accessToken);
+        $this->response = $this->client->delete($this->graphVersion.$path.$separator.'access_token='.$this->accessToken);
 
         return $this;
     }
