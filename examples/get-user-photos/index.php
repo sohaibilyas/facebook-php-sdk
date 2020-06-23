82beab4d6eec2b8f3ae33360cd7f12ab57028fa2
Sohaib Ilyas <contact@sohaibilyas.com>
2020-06-24 01:01:28 +0500

    added user photos example

---
+++ examples/get-user-photos/index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+require './vendor/autoload.php';
+
+use SohaibIlyas\FacebookPhpSdk\Facebook;
+
+$facebook = new Facebook([
+    'app_id' => 'your_app_id_here',
+    'app_secret' => 'your_app_secret_here',
+    'redirect_uri' => 'your_redirect_uri_here',
+]);
+
+if ($facebook->loggedIn() || isset($_SESSION['access_token'])) {
+    if (isset($_SESSION['access_token'])) {
+        $facebook->setAccessToken($_SESSION['access_token']);
+    }
+
+    $_SESSION['access_token'] = $facebook->getAccessToken();
+
+    $response = $facebook->get('/me/photos/uploaded')->toArray();
+
+    print_r($response);
+} else {
+    echo "<a href='" . $facebook->getLoginUrl(['email', 'user_photos']) . "'>Login with Facebook</a>";
+}
